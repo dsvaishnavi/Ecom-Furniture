156c448193795d281cf5a4ca9403acaa1614fd4d
Satyajeet <sattudesai007@gmail.com>
2025-04-30 03:17:27 +0530

    Initial Commit

--- client/src/components/CategoryList.jsx
+++ client/src/components/CategoryList.jsx
@@ -1,9 +1,29 @@
-export const CategoryList = () => { 
+import { use, useContext , useEffect } from "react";
+import { CategoryContext } from "../context/CategoryContext";
+
+
+
+export const CategoryList = () => {
+    const { category, fetchCategory } = useContext(CategoryContext);
+    console.log(category);
+
+    useEffect(() => {
+        fetchCategory();
+    }, []);
+
     return (
-         <div className="flex flex-col items-center justify-center bg-[#ffff] py-16 mt-20">
-            <div className="flex items-center justify-center mb-04">
-                
+        <div className="flex flex-col gap-5 mt-10 bg-[#ffff] flex-direction row h-200vh w-full justify-center items-center mb-10">
+            <h1 className="text-3xl font-bold text-[#523a28]">Our Products..</h1>
+            <div className="flex flex-wrap gap-5 justify-center">
+                {category?.map((item, index) => {
+                    return (
+                        <div key={index} className="flex flex-col items-center justify-center gap-2 ">
+                            <img src={item.image} alt={item.name} className="w-[100px] h-[100px] mb-2" />
+                            <h2 className="text-xl font-semibold text-[#523a28]">{item.name}</h2>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
-}   
+}
