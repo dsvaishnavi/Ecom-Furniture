fef0252b0521a1addb3188e5ac3b32b8ff39a9b9
Satyajeet <sattudesai007@gmail.com>
2025-05-03 12:26:42 +0530

    Img seting up

--- client/src/components/ImageHome.jsx
+++ client/src/components/ImageHome.jsx
@@ -8,7 +8,7 @@ export const ImageHome = () => {
   // const unsplash_client = process.env.UNSPLASH_CLIENT_ID;
   useEffect(() => {
     fetch(
-      `https://api.unsplash.com/search/photos?query=furniture&per_page=14&client_id=ZmN_8OeBsphKWWdlG4m25aecFL27dalv0vdcPayFVtQ`
+      `https://api.unsplash.com/search/photos?query=furniture&per_page=18&client_id=ZmN_8OeBsphKWWdlG4m25aecFL27dalv0vdcPayFVtQ`
     )
       .then((res) => {
         if (!res.ok) {
@@ -26,19 +26,23 @@ export const ImageHome = () => {
     <div>
       <div>
         <h1 className="text-4xl text-center ml-5 mb-5 font-bold animate-scroll inline-block text-[#523a28] mt-7 ">
-          What's New at UrbanNest ?
+          What's New at UrbanNest..?
         </h1>   
       </div>
 
-      <div className=" md:columns-3 lg:columns-5 gap-4 p-4 ">
+      <div className="md:columns-3 lg:columns-4 gap-4 p-4 min-h-screen">
         {images.map((img) => (
           <img
             key={img.id}
             src={img.urls.small}
             alt={img.alt_description || "Random Unsplash"}
-            className="mb-3  w-full  rounded-sm hover:scale-105 transition duration-200 ease-in "
+            className="mb-3 w-full hover:scale-105 rounded-sm transition duration-200 ease-in"
           />
+          
         ))}
+        <h1 className="text-4xl text-center ml-5 mb-5 font-bold animate-scroll inline-block text-[#523a28] mt-7 ">
+          Explore Our Collections....
+        </h1>
       </div>
     </div>
   );
